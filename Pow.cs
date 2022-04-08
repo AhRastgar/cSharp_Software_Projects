2b2ed5a4caa49fa3bb9e5ddcc35b7d0e07704c22
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-04-08 16:46:42 +0430

    c# projects

---
+++ Pow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // subtitle
+            Console.WriteLine("This project is for pow.");
+
+            // variables & input
+            Console.WriteLine("Enter a number for pow ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            // operation & output
+            Console.WriteLine("pow 1: " + a);
+            Console.WriteLine("pow 2: " + a * a);
+            Console.WriteLine("pow 3: " + a * a * a);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ x Pow y.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_Pow_y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // subtitle
+            Console.WriteLine("This project is for x pow y.");
+
+            // variables & input
+            Console.WriteLine("Enter first number ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int stash = 1;
+
+            // operation
+            for (int i = 0; i < y; i++)
+            {
+                stash *= x;
+            }
+
+            // output
+            Console.WriteLine("x pow y: " + stash);
+
+            Console.ReadKey();
+        }
+    }
+}

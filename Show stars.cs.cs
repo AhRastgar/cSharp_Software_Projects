7c874c96a7546971a243fed712aa085ee4c889c8
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-20 18:13:47 +0330

    Show stars

---
+++ Show stars.cs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show_stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for show stars.");
+
+            Console.WriteLine("Enter number of stars for draw a star: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

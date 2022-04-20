52f4e1a8e5a1167a2b958ba3275bd3e7524c303e
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-20 18:05:45 +0330

    Golden number

---
+++ Golden Number.cs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golden_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for golden number.");
+
+            Console.WriteLine("enter number to Divine Proportion: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y, z;
+
+            y = (int)(x / 1.618);
+            z = (int)(x * 1.618);
+
+            Console.WriteLine("If your number is length, the ratio is: " + y);
+            Console.WriteLine("If your number is width, the ratio is: " + z);
+
+            Console.ReadKey();
+        }
+    }
+}

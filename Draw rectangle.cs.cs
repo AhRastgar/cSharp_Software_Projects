a6a5fe6a17ef369ad04f219907005921d9cf8725
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-20 18:32:33 +0330

    Draw rectangle

---
+++ Draw rectangle.cs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for draw rectangle.");
+
+            Console.WriteLine("Enter length of rectangle ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter width of rectangle ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

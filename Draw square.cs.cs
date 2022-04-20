7ca6279fed611ef4c6385573561524858031df1d
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-20 18:28:13 +0330

    Draw square

---
+++ Draw square.cs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for draw square.");
+
+            Console.WriteLine("Enter a number for side of square ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

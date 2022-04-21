3ae476d5172193e83eb05087cef676d5fb2f5d2b
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-21 12:57:11 +0330

    Complete number

---
+++ Complete number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compelete_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for complete number.");
+
+            Console.WriteLine("Enter your number ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int num = x;
+
+            int sum = 0;
+
+            for (int i = 1; i < x; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if (sum == x)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

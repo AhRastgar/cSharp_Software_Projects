6fb2f0baa28100dad9d65a601e56d05dae9b00e5
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-21 13:38:02 +0330

    Draw triangle O

---
+++ Draw triangle O.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_triangle_O
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for draw triangle O.");
+
+            Console.WriteLine("Enter number for side of triangle ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+                for (int k = i; k <= x; k++)
+                {
+                    Console.Write("  ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

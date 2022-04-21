c61c4688397cce2fe126a6cbde35b19de44d8d4e
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-21 13:21:08 +0330

    Draw triangle A

---
+++ Draw triangle A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_triangle_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for draw triangle A.");
+
+            Console.WriteLine("Enter number for side of triangle ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}

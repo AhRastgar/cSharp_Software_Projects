a1d04c1332148333fcfc5e75597fbdb8512cd48a
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-22 09:37:45 +0330

    Diameter square

---
+++ Diameter square.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hollow_square
+{
+    internal class Program
+    {
+        static void square(int length)
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 1; j <= length; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if (j == (length + 1) - i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for hollow square.");
+
+            Console.WriteLine("Enter number for side of square ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            square(x);
+
+            Console.ReadKey();
+        }
+    }
+}

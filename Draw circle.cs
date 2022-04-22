9f81e5f1188afa9377cbef3edb0b8d6234c061df
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-22 12:33:25 +0330

    Draw circle

---
+++ Draw circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for draw circle.");
+
+            Console.WriteLine("Enter radius of the circle ");
+            float x = Convert.ToInt32(Console.ReadLine());
+            float k;
+
+            float y = 2F;
+
+            for (float i = -x; i <= x; i++)
+            {
+                for (float j = -x; j <= x; j++)
+                {
+                    k = (((i * y) / x) * ((i * y) / x) + (j / x) * (j / x));
+                    if (k > 0.95 && k < 1.08)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

6e1f4718d4abfebb218fd4c777e095b7e4326015
AhRastgar <ah.rastgar.c@gmail.com>
2022-04-22 08:42:38 +0330

    Hollow rectangle

---
+++ Hollow rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hollow_rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is for hollow rectangle.");
+
+            Console.WriteLine("Enter length of rectangle ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter width of rectangle ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < y - 2; i++)
+            {
+                Console.Write("*");
+                for (int j = 0; j < 2 * x - 3; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
